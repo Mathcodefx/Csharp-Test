81d98a2e221f05a95aeb7907a275b4e019bef128
Mathcodefx <matheustempo21@gmail.com>
2022-11-28 22:34:08 -0300

    Exerc Vet

---
+++ ExercVet/ExercVet/Info.cs
@@ -0,0 +1,21 @@
+﻿namespace ExercVet
+{
+    class Info
+    {
+        public int NumeroQuarto { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public Info(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", " + Email;
+        }
+
+    }
+}
---
+++ ExercVet/ExercVet/Program.cs
@@ -0,0 +1,48 @@
+﻿/* Exercicio criando um vet e armazenando dados neles, depois mostrar os dados em ordem crescente */
+
+
+using ExercVet;
+
+namespace RoomVet
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Info[] vet = new Info[10];
+
+            Console.WriteLine("Quantos quartos serao ocupados? ");
+            int n=int.Parse(Console.ReadLine());
+
+            for(int i=1; i<=n; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Aluguel #{i}:");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+                vet[quarto] = new Info(nome, email);
+
+            }
+            Console.WriteLine();
+            Console.Write("Busy Rooms");
+
+            for (int i = 1; i < 10; i++)
+            {
+                if (vet[i] != null)
+                {
+                    Console.WriteLine(i + ": " + vet[i]);
+                }
+            }        
+
+
+            }
+
+        }
+
+    }
+
+
