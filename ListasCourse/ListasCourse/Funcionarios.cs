3bef9d91d2a36582c36bd9e94d68c590fc4a5dd7
Mathcodefx <matheustempo21@gmail.com>
2022-12-14 18:21:10 -0300

    Testando listas

---
+++ ListasCourse/ListasCourse/Funcionarios.cs
@@ -0,0 +1,35 @@
+﻿
+using System.Globalization;
+
+namespace ListasCourse
+{
+    class Funcionarios
+    {
+       public string Nome { get; set; }
+       public  double Salario { get; private set; }
+       public int Id { get; set; }
+
+     public Funcionarios(string nome, double salario, int id)
+        {
+            Id = id;
+            Salario = salario;
+            Nome = nome;
+        }
+
+        public void AumentarSalario(double porcentagem)
+        {
+            
+             Salario+=Salario*porcentagem/100.0;
+        }
+
+      
+        public override string ToString()
+        {
+            return
+            "Id: " + Id + ", Nome:" + Nome
+                + ", Salario: $" + Salario.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+
+    }
+}
---
+++ ListasCourse/ListasCourse/Program.cs
@@ -0,0 +1,66 @@
+﻿/* Fazer um programa para ler um numero inteiro de dados (id, nome e salario) de funcionarios. Não pode haver repetição de id.
+ * Depois efetuar um aumento no salarios atraves da id */
+
+
+using ListasCourse;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(String[] Args)
+        {
+            Console.WriteLine("Quantos funcionarios serao registrados? ");
+            int n = int.Parse(Console.ReadLine());
+
+            List<Funcionarios> list = new List<Funcionarios>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("Funcionario # " + i + ":");
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Salario: ");
+                double salario = double.Parse(Console.ReadLine());
+                list.Add(new Funcionarios(nome, salario, id));
+            }
+            Console.WriteLine();
+
+
+            Console.Write("Entre com o empregado que ira receber a porcentagem: ");
+            int searchId = int.Parse(Console.ReadLine());
+
+            Funcionarios emp = list.Find(x => x.Id == searchId);
+            if (emp != null)
+            {
+                Console.Write("Entre com a porcentagem: ");
+                double percentage = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                emp.AumentarSalario(percentage);
+            }
+            else
+            {
+                Console.WriteLine("Essa id não existe!");
+            }
+
+            Console.WriteLine("Lista atualizada de empregados:");
+            foreach (Funcionarios obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+
+
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
